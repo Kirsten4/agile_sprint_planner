f2445c68c5dddb14c7b5170b47e9033d3d269120
Kirsten Buchan <kirsten2412@hotmail.co.uk>
2022-02-22 16:58:09 +0000

    styled components added

--- client/src/components/task_lists/Column.js
+++ client/src/components/task_lists/Column.js
@@ -1,26 +1,43 @@
 import { Droppable } from 'react-beautiful-dnd';
+import styled from 'styled-components';
 import Task from './Task';
 
-const Column = ({column, tasks}) => {
+const Container = styled.div`
+    margin: 8px;
+    border: 1px solid lightgrey;
+    border-radius: 2px;
+    width: 200px;
+    display: flex;
+    flex-direction: column;
+    `;
+
+const TaskList = styled.div`
+    padding: 8px;
+    background-color: skyblue;
+    flex-grow: 1;
+    min-height: 100px;
+    `;
+
+const Column = ({ column, tasks }) => {
 
     return (
-        <>
-        <h3>{column.title}</h3>
-        <Droppable droppableId={column.id}>
-            {(provided, snapshot) => (
-                <div 
-                ref={provided.innerRef}
-                {...provided.droppableProps}
-                // isDraggingOver={snapshot.isDraggingOver}
-                >
-                {tasks.map((task, index) => (
-                    <Task key={task.id} task={task} index={index} />
-                ))}
-                {provided.placeholder}
-                </div>
-            )}
-        </Droppable>
-        </>
+        <Container>
+            <h3>{column.title}</h3>
+            <Droppable droppableId={column.id}>
+                {(provided, snapshot) => (
+                    <TaskList
+                        ref={provided.innerRef}
+                        {...provided.droppableProps}
+                        // isDraggingOver={snapshot.isDraggingOver}
+                    >
+                        {tasks.map((task, index) => (
+                            <Task key={task.id} task={task} index={index} />
+                        ))}
+                        {provided.placeholder}
+                    </TaskList>
+                )}
+            </Droppable>
+        </Container>
     )
 }
 
--- client/src/components/task_lists/Task.js
+++ client/src/components/task_lists/Task.js
@@ -1,21 +1,30 @@
 import { Draggable } from 'react-beautiful-dnd';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    border: 1px solid lightgrey;
+    border-radius: 2px;
+    padding 8px;
+    margin-bottom: 8px;
+    background-color: ${props => (props.isDragging ? 'lightgreen' : 'white')};
+`
 
 const Task = ({task, index}) => {
     
     return (
         <Draggable draggableId={task.id} index={index}>
             {(provided, snapshot) => (
-                <div
+                <Container
                     ref={provided.innerRef}
                     {...provided.draggableProps}
                     {...provided.dragHandleProps}
                     // innerRef={provided.innerRef}
                     
                     
-                    // isDragging={snapshot.isDragging}
+                    isDragging={snapshot.isDragging}
                     >
                     {task.content}
-                </div>
+                </Container>
             )}
         </Draggable>
     );
